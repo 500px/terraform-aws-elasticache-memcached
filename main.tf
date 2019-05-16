789a0b72fd45e399159555c31cb959e73e9eb573
Larry Li <larry@500px.com>
2019-05-15 21:12:08 -0400

    prefix for parameter_group and subnet_group

--- main.tf
+++ main.tf
@@ -44,12 +44,12 @@ resource "aws_security_group" "default" {
 }
 
 resource "aws_elasticache_subnet_group" "default" {
-  name       = "${module.label.id}"
+  name       = "memcached-${module.label.id}"
   subnet_ids = ["${var.subnets}"]
 }
 
 resource "aws_elasticache_parameter_group" "default" {
-  name   = "${module.label.id}"
+  name   = "$memcached-{module.label.id}"
   family = "memcached1.4"
 
   parameter {
